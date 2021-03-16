ce90f84fd20fc8fa35edb18e9c9f876859ba0f8a
Moses Buckwalter <mosesbuckwalter@users.com>
2021-03-16 08:46:10 -0400

    Add so the input is taken in a while loop.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 print('Your computer will restart now, please answer yes or no to continue.')
-n = input()
-if n == "no":
-    print('Your computer will not restart.')
-elif n == "yes":
-    print('Your computer will now restart.')
-    from subprocess import call
-    rc = call("./reboot.sh")
-elif n != "yes" or "no":
+n = 0
+while n != "yes" or "no":
+    n = input()
     print('Please try again')
+    if n == "no":
+        print('Your computer will not restart.')
+        exit()
+    elif n == "yes":
+        print('Your computer will now restart.')
+        from subprocess import call
+        rc = call("./reboot.sh")
