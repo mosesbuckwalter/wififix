ab4a798980c5108ad3cbb0a1a5133bd7aa83d234
Moses Buckwalter <mosesbuckwalter@users.com>
2021-03-16 08:26:31 -0400

    Add reboot.sh to force reboot. Add recieves user input and asks yes or no.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('This should now be on GitHub')
+print('Your computer will restart now, please answer yes or no to continue.')
+n = input()
+if n == "no":
+    print('Your computer will not restart.')
+elif n == "yes":
+    print('Your computer will now restart.')
+    from subprocess import call
+    rc = call("./reboot.sh")
+elif n != "yes" or "no":
+    print('Please try again')
