98f3f71a0698e3036b58ba4e6543cfa4cde6b397
Moses Buckwalter <mosesbuckwalter@users.com>
2021-03-18 08:45:21 -0400

    Change code into functions.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from urllib.request import urlopen
+def availablity():
+    try:
+        urlopen('http://216.58.192.142', timeout=1)
+        return True
+        time.sleep(5.0)
+    except:
+        return False
 print('How would you like to handle poor connection? Say "reboot" if you would like to restart your computer if internet connection is bad. Say "reset" if you would like the WiFi connection to be turned off and on when the connection is poor.')
 n = 0
 while n != "reboot" or "reset":
@@ -12,14 +20,6 @@ while n != "reboot" or "reset":
 import time
 if x == 1:
     while x == 1:
-        from urllib.request import urlopen
-        def availablity():
-            try:
-                urlopen('http://216.58.192.142', timeout=1)
-                return True
-                time.sleep(5.0)
-            except:
-                return False
         if availablity() == False:
             print('Your computer will restart now, please answer yes or no to continue.')
             n = 0
@@ -36,14 +36,6 @@ if x == 1:
                 print('Please try again')
 elif x == 2:
     while x == 2:
-        from urllib.request import urlopen
-        def availablity():
-            try:
-                urlopen('http://216.58.192.142', timeout=1)
-                return True
-                time.sleep(5.0)
-            except:
-                return False
         if availablity() == False:
             print('Your computer will now turn the WiFi off and on again.')
             import os
