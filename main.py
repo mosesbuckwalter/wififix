c2c44cd2ce294fd7c5aabc2db3e4006a9305587b
Moses Buckwalter <mosesbuckwalter@users.com>
2021-03-19 07:54:38 -0400

    Add comments

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from urllib.request import urlopen
-def availablity():
+def availablity(): #this function trys to connect to Google, and if it does successfully, then it returns true, otherwise it will return false.
     try:
         urlopen('http://216.58.192.142', timeout=1)
         return True
         time.sleep(5.0)
     except:
         return False
-def wifi():
+def wifi(): #this function turns off and on the wifi again and prints status messages.
     print('Your computer will now turn the WiFi off and on again.')
     cmd = 'nmcli radio wifi off' #turn off WiFi
     os.system(cmd)
     time.sleep(5.0)
-    cmd = 'nmcli radio wifi on'
+    cmd = 'nmcli radio wifi on' #turn on WiFi
     os.system(cmd)
     time.sleep(20.0)
-    print('WiFi successfully turned off and on')
-def reboot():
+    print('WiFi successfully turned off and on') #indication of it working
+def reboot(): #this program restarts your computer when WiFi is detected to be not connected, and takes a yes or no input from the user.
     print('Your computer will restart now, please answer yes or no to continue.')
     n = 0
     while n != "yes" or "no":
@@ -28,7 +28,7 @@ def reboot():
             cmd = 'sudo reboot'
             os.system(cmd)
         print('Please try again')
-def confirm():
+def confirm(): #this function takes user input and requires a yes or no response and will loop until it receives such.
     global n
     n = 0
     while n != "no" or "yes":
@@ -38,12 +38,12 @@ def confirm():
         elif n == "no":
             exit()
         print('Please try again')
-import time
-import os
+import time #used for the sleep function.
+import os #used to run os commands
 
 
-print('This program will turn off and on your WiFi when it detects poor signal. Are you okay with this? Answer "yes" or "no".')
-confirm()
+print('This program will turn off and on your WiFi when it detects poor signal. Are you okay with this? Answer "yes" or "no".') #stating what the program will do when ran.
+confirm() #confirms that the user would like to do this.
 while n == "yes":
-    if availablity() == False:
-        wifi()
+    if availablity() == False: #checks if WiFi connected is false
+        wifi() #if the WiFi connected is false, it will restart the WiFi.
